a3d15be8cdd57dd2cef2013ff2c2ff4d79577a28
Sruthi556 <63460103+Sruthi556@users.noreply.github.com>
2020-04-25 13:16:39 +0530

    Add files via upload

---
+++ ss.html
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 17:32:47 2020
+
+@author: Sruthi
+"""
+
+print("hello world!")
---
+++ v.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 17:43:22 2020
+
+@author: Sruthi
+"""
+
+import numpy as nm
+import matplotlib.pyplot as mtp
+import pandas as py
